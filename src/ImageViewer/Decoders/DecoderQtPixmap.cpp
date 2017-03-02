00b35fcf0aa0ffea5327a87765a9c4562c716304
Peter Zhigalov <peter.zhigalov@gmail.com>
2017-03-03 03:32:07 +0700

    Add EXIF Orientation support to DecoderQtPixmap

--- src/ImageViewer/Decoders/DecoderQtPixmap.cpp
+++ src/ImageViewer/Decoders/DecoderQtPixmap.cpp
@@ -25,6 +25,11 @@
 #include <QGraphicsPixmapItem>
 #include <QPixmap>
 #include <QFileInfo>
+#include <QDebug>
+
+#if defined (HAS_THIRDPARTY_QTEXTENDED)
+#include "qexifimageheader.h"
+#endif
 
 #include "DecoderAutoRegistrator.h"
 
@@ -33,6 +38,52 @@ namespace {
 const int DECODER_QT_PIXMAP_PRIORITY = 100;
 
 DecoderAutoRegistrator registrator(new DecoderQtPixmap, true);
+
+#if defined (HAS_THIRDPARTY_QTEXTENDED)
+
+void applyExifOrientation(QImage *image, quint16 exifOrientation)
+{
+    // This is not an optimized implementation, but easiest to maintain
+    QTransform transform;
+
+    switch (exifOrientation) {
+        case 1: // normal
+            break;
+        case 2: // mirror horizontal
+            *image = image->mirrored(true, false);
+            break;
+        case 3: // rotate 180
+            transform.rotate(180);
+            *image = image->transformed(transform);
+            break;
+        case 4: // mirror vertical
+            *image = image->mirrored(false, true);
+            break;
+        case 5: // mirror horizontal and rotate 270 CCW
+            *image = image->mirrored(true, false);
+            transform.rotate(270);
+            *image = image->transformed(transform);
+            break;
+        case 6: // rotate 90 CW
+            transform.rotate(90);
+            *image = image->transformed(transform);
+            break;
+        case 7: // mirror horizontal and rotate 90 CW
+            *image = image->mirrored(true, false);
+            transform.rotate(90);
+            *image = image->transformed(transform);
+            break;
+        case 8: // rotate 270 CW
+            transform.rotate(-90);
+            *image = image->transformed(transform);
+            break;
+        default:
+            qWarning("This should never happen");
+    }
+    exifOrientation = 1;
+}
+
+#endif
 
 } // namespace
 
@@ -78,7 +129,29 @@ QGraphicsItem *DecoderQtPixmap::loadImage(const QString &filename)
     const QFileInfo fileInfo(filename);
     if(!fileInfo.exists() || !fileInfo.isReadable())
         return NULL;
-    QPixmap pixmap(filename);
+    QPixmap pixmap;
+    bool status = false;
+
+#if defined (HAS_THIRDPARTY_QTEXTENDED)
+    QExifImageHeader exifHeader;
+    status = exifHeader.loadFromJpeg(filename) && exifHeader.contains(QExifImageHeader::Orientation);
+    if(status)
+    {
+        quint16 orientation = exifHeader.value(QExifImageHeader::Orientation).toShort();
+        if((status = (orientation >= 2 && orientation <= 8)))
+        {
+            QImage image(filename);
+            if((status = !image.isNull()))
+            {
+                applyExifOrientation(&image, exifHeader.value(QExifImageHeader::Orientation).toShort());
+                pixmap = QPixmap::fromImage(image);
+            }
+        }
+    }
+#endif
+
+    if(!status)
+        pixmap.load(filename);
     if(pixmap.isNull())
         return NULL;
     return new QGraphicsPixmapItem(pixmap);
