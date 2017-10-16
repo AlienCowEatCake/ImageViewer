424d6904f306501e19c16f455848adf6f608a028
Peter Zhigalov <peter.zhigalov@gmail.com>
2017-10-17 00:43:22 +0700

    Deploy with MSVC 2017 + Qt 5.6.3 (Windows)

--- buildscripts/win_qt5.6_msvc2017_x64.bat
+++ buildscripts/win_qt5.6_msvc2017_x64.bat
@@ -1,10 +1,10 @@
 @echo off
 set PROJECT=ImageViewer
-set ARCH=x86
-set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-set QTDIR=C:\Qt\5.6.2\msvc2015_static
-set BUILDDIR=build_win_qt5.6_msvc2015_%ARCH%
-set SUFFIX=_qt5.6_msvc2015_%ARCH%
+set ARCH=x64
+set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
+set QTDIR=C:\Qt\5.6.3\msvc2017_64_static
+set BUILDDIR=build_win_qt5.6_msvc2017_%ARCH%
+set SUFFIX=_qt5.6_msvc2017_%ARCH%
 set APP_PATH=src\%PROJECT%
 
 call %VCVARS% %ARCH%
