d29c8462482cedc3248651c6cebe06833f0d9bfe
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-06-28 19:15:09 +0700

    Bump version to 1.1.0

--- src/ImageViewer/src/main.cpp
+++ src/ImageViewer/src/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain(QString::fromLatin1("fami.codefreak.ru"));
     app.setOrganizationName(QString::fromLatin1("FAMI Net Team"));
     app.setApplicationName(QString::fromLatin1("Image Viewer"));
-    app.setApplicationVersion(QString::fromLatin1("1.0.1"));
+    app.setApplicationVersion(QString::fromLatin1("1.1.0"));
 #if !defined (Q_OS_MAC)
     app.setWindowIcon(QIcon(QString::fromLatin1(":/icon/icon.ico")));
     app.setAttribute(Qt::AA_DontShowIconsInMenus, false);
