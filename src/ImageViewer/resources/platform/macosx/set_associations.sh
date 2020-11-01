f2f11c5f3537e3e29c4ae962a7d2052964a1736d
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-11-01 19:23:50 +0700

    Fix assocications script on macOS Big Sur 11.0.1 Beta

--- src/ImageViewer/resources/platform/macosx/set_associations.sh
+++ src/ImageViewer/resources/platform/macosx/set_associations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 BUNDLE_ID="ru.codefreak.fami.imageviewer"
-APP_PATH=$(osascript -e "POSIX path of (path to application id \"${BUNDLE_ID}\")")
+APP_PATH=$(osascript -e "POSIX path of (path to application id \"${BUNDLE_ID}\")" | sed 's|:/$||')
 for ext in $(cat "${APP_PATH}/Contents/Info.plist" | egrep '\t\t\t\t<string>[a-z0-9]{3,5}</string>' | sed 's|.*<string>\(.*\)</string>|\1|') ; do
 	duti -s ${BUNDLE_ID} ${ext} viewer
 done
