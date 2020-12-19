501a2dcabf13333ce4e33df270fd88395c68678f
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-12-20 03:05:15 +0700

    Refresh application icon

--- src/ImageViewer/resources/icon/png2icns.sh
+++ src/ImageViewer/resources/icon/png2icns.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 pngdir="${PWD}"
 iconset="icon.iconset"
--- src/ImageViewer/resources/icon/svg2png.sh
+++ src/ImageViewer/resources/icon/svg2png.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if [ ! -z "$(uname -s | grep -E -i 'darwin')" ] ; then
+	export PATH="${PATH}:/Applications/Inkscape.app/Contents/MacOS"
+	export PATH="${PATH}:/Applications/GIMP-2.10.app/Contents/MacOS"
+fi
 
 sizes="16 32 48 64 128 256 512 1024"
 
 for size in ${sizes} ; do
 	name="icon_${size}"
-	inkscape -z -e ${PWD}/${name}.png -w ${size} -h ${size} ${PWD}/drawing.svg
+	inkscape -z -e "${PWD}/${name}.png" -w ${size} -h ${size} "${PWD}/drawing.svg" || inkscape -C -o "${PWD}/${name}.png" -w ${size} -h ${size} "${PWD}/drawing.svg"
 	#gimp -c -d -i -b "(let* ((image (car (file-png-load 0 \"${name}.png\" \"${name}.png\"))) (drawable (car (gimp-image-get-active-layer image)))) (gimp-image-convert-indexed image 0 0 255 0 1 \"\") (file-png-save 1 image drawable \"${name}.png\" \"${name}.png\" 0 9 0 0 0 0 0) )" -b "(gimp-quit 0)"
 	optipng -o7 -zm1-9 -strip all ${name}.png
 done
