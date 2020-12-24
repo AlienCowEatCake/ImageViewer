584f487c44e83d07d63bd46a984f5834c828f0c3
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-12-24 23:04:46 +0700

    Switch Windows deploy scripts to Qt 5.15.2

--- buildscripts/win_qt5.15_msvc2019_x64_sln.bat
+++ buildscripts/win_qt5.15_msvc2019_x64_sln.bat
@@ -2,7 +2,7 @@
 set PROJECT=ImageViewer
 set ARCH=x64
 set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
-set QTDIR=C:\Qt\5.15.0\msvc2019_64_static
+set QTDIR=C:\Qt\5.15.2\msvc2019_64_static
 set BUILDDIR=build_win_qt5.15_msvc2019_%ARCH%_sln
 
 call %VCVARS% %ARCH%
