28dac0a2c3a9011f3435e91792fc6f41d9bc8594
Peter Zhigalov <peter.zhigalov@gmail.com>
2020-12-25 07:51:14 +0700

    Add deploy with Docker for GNU/Linux

---
+++ buildscripts/docker_linux_qt5.15_clang.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+SOURCE_DIR="$(cd "$(dirname "${0}")/.." && pwd)"
+FORWARD_SCRIPT="${0##*/docker_}"
+DOCKER_IMAGE="aliencoweatcake/amd64-trusty-qt5projects:qt5.15.2"
+DOCKER_USER="user"
+DOCKER_WORKSACE="/home/${DOCKER_USER}/workspace"
+
+ADDITIONAL_DOCKER_ARGS=
+if [ -t 1 ]; then
+	if [ -z "$(uname -s | grep -E -i 'cygwin|mingw|msys')" ]; then
+		ADDITIONAL_DOCKER_ARGS="${ADDITIONAL_DOCKER_ARGS} --tty"
+	fi
+fi
+
+docker pull "${DOCKER_IMAGE}"
+MSYS_NO_PATHCONV=1 docker run \
+	--rm \
+	--interactive \
+	--user "${DOCKER_USER}" \
+	--volume "${SOURCE_DIR}:${DOCKER_WORKSACE}:delegated" \
+	${ADDITIONAL_DOCKER_ARGS} \
+	"${DOCKER_IMAGE}" \
+	"${DOCKER_WORKSACE}/buildscripts/${FORWARD_SCRIPT}"
