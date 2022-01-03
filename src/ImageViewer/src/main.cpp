9a87b02772f0bb93db0ad31513783c52634e078f
Peter Zhigalov <peter.zhigalov@gmail.com>
2022-01-04 04:15:30 +0700

    Bump version to 1.4.4

--- src/ImageViewer/src/main.cpp
+++ src/ImageViewer/src/main.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2017-2021 Peter S. Zhigalov <peter.zhigalov@gmail.com>
+   Copyright (C) 2017-2022 Peter S. Zhigalov <peter.zhigalov@gmail.com>
 
    This file is part of the `ImageViewer' program.
 
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain(QString::fromLatin1("aliencoweatcake.github.com"));
     app.setOrganizationName(QString::fromLatin1("AlienCowEatCake"));
     app.setApplicationName(QString::fromLatin1("Image Viewer"));
-    app.setApplicationVersion(QString::fromLatin1("1.4.3"));
+    app.setApplicationVersion(QString::fromLatin1("1.4.4"));
 #if !defined (Q_OS_MAC)
     app.setWindowIcon(QIcon(QString::fromLatin1(":/icon/icon.ico")));
     app.setAttribute(Qt::AA_DontShowIconsInMenus, false);
