738a8a59bc3ba4990c0006b92412e695dbdb3f93
Peter Zhigalov <peter.zhigalov@gmail.com>
2022-04-28 10:25:49 +0700

    Fix MSEdgeWebView2 version in deploy scripts

--- buildscripts/win_qt5.15_msvc2022_x64.bat
+++ buildscripts/win_qt5.15_msvc2022_x64.bat
@@ -12,7 +12,7 @@ set BUILDDIR=build_win_qt5.15_msvc2022_%ARCH%
 set SUFFIX=_qt5.15_msvc2022_%ARCH%
 set APP_PATH=src\%PROJECT%
 set ZIP_CMD="%~dp0\..\buildscripts\helpers\zip.exe"
-set WEBVIEW2LOADER_DLL="%~dp0\..\src\ThirdParty\MSEdgeWebView2\microsoft.web.webview2.1.0.1108.44\build\native\%ARCH%\WebView2Loader.dll"
+set WEBVIEW2LOADER_DLL="%~dp0\..\src\ThirdParty\MSEdgeWebView2\microsoft.web.webview2.1.0.1185.39\build\native\%ARCH%\WebView2Loader.dll"
 
 call %VCVARS% %VCVARS_ARCH%
 set PATH=%QTDIR%\bin;%WIX_DIR%\bin;%PATH%
