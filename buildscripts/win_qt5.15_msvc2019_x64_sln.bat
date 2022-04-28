05c75c13e205efc5cdd0d35c011631e38a2eee77
Peter Zhigalov <peter.zhigalov@gmail.com>
2022-04-28 10:28:37 +0700

    Update solution buildscript

--- buildscripts/win_qt5.15_msvc2019_x64_sln.bat
+++ buildscripts/win_qt5.15_msvc2019_x64_sln.bat
@@ -2,7 +2,7 @@
 set PROJECT=ImageViewer
 set ARCH=x64
 set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
-set QTDIR=C:\Qt\5.15.2\msvc2019_64_static
+set QTDIR=C:\Qt\5.15.2\msvc2019_64
 set BUILDDIR=build_win_qt5.15_msvc2019_%ARCH%_sln
 
 call %VCVARS% %ARCH%
@@ -13,6 +13,6 @@ cd ..
 rmdir /S /Q %BUILDDIR% 2>nul >nul
 mkdir %BUILDDIR%
 cd %BUILDDIR%
-qmake -r CONFIG+="release" QTPLUGIN.imageformats="qico qsvg qtiff" CONFIG+="enable_update_checking" CONFIG+="disable_msedgewebview2" CONFIG+="enable_librsvg enable_resvg enable_magickwand enable_graphicsmagickwand enable_qtextended" -tp vc ..\%PROJECT%.pro
+qmake -r CONFIG+="release" CONFIG+="enable_update_checking" CONFIG+="enable_librsvg enable_resvg enable_magickwand enable_graphicsmagickwand enable_qtextended enable_qtwebkit enable_qtwebengine enable_qmlwebengine" -tp vc ..\%PROJECT%.pro
 
 pause
