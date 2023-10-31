1d3526c18c4feadf791dbac098d9bf4fc54d1ae0
Peter Zhigalov <peter.zhigalov@gmail.com>
2023-11-01 05:18:06 +0700

    Add buildscript for Windows with Qt 6.5.3

---
+++ buildscripts/win_qt6.5_msvc2022_x64.bat
@@ -0,0 +1,50 @@
+@echo off
+set PROJECT=ImageViewer
+set ARCH=x64
+set VCVARS_ARCH=x64
+call "%~dp0\..\buildscripts\helpers\find_vcvarsall.bat" 2022
+set VCVARS="%VS2022_VCVARSALL%"
+if "x%QT_PATH%x" == "xx" set QT_PATH=C:\Qt\6.5.3\msvc2019_64
+set BUILDDIR=build_win_qt6.5_msvc2022_%ARCH%
+set SUFFIX=_qt6.5_msvc2022_%ARCH%
+set APP_PATH=src\%PROJECT%
+set NMAKE_CMD="%~dp0\..\buildscripts\helpers\jom.exe" /J %NUMBER_OF_PROCESSORS%
+set ZIP_CMD="%~dp0\..\buildscripts\helpers\zip.exe"
+set DLLRESOLVER_CMD="%~dp0\..\buildscripts\helpers\dllresolver.exe"
+set WEBVIEW2LOADER_DLL="%~dp0\..\src\ThirdParty\MSEdgeWebView2\microsoft.web.webview2.1.0.1823.32\build\native\%ARCH%\WebView2Loader.dll"
+set RESVG_PATH="%~dp0\resvg\x86_64-pc-windows-msvc"
+
+call %VCVARS% %VCVARS_ARCH%
+set PATH=%QT_PATH%\bin;%WIX%\bin;%WIX%;%PATH%
+set CRT_DIR="%VCToolsRedistDir%\x64\Microsoft.VC143.CRT"
+set UCRT_DIR="%UniversalCRTSdkDir%\Redist\%UCRTVersion%\ucrt\DLLs\x64"
+
+cd "%~dp0"
+cd ..
+rmdir /S /Q %BUILDDIR% 2>nul >nul
+mkdir %BUILDDIR%
+cd %BUILDDIR%
+qmake -r CONFIG+="release" CONFIG+="enable_update_checking" CONFIG+="system_resvg" INCLUDEPATH+=%RESVG_PATH% LIBS+=/LIBPATH:%RESVG_PATH% ..\%PROJECT%.pro
+%NMAKE_CMD%
+if not exist %APP_PATH%\release\%PROJECT%.exe (
+    if NOT "%CI%" == "true" pause
+    exit /b 1
+)
+rmdir /S /Q %PROJECT%%SUFFIX% 2>nul >nul
+mkdir %PROJECT%%SUFFIX%
+copy %APP_PATH%\release\%PROJECT%.exe %PROJECT%%SUFFIX%\%PROJECT%.exe
+windeployqt --release --no-compiler-runtime --no-system-d3d-compiler --no-virtualkeyboard --no-pdf --no-opengl-sw --translations en,ru %PROJECT%%SUFFIX%
+copy %WEBVIEW2LOADER_DLL% %PROJECT%%SUFFIX%\
+%DLLRESOLVER_CMD% %PROJECT%%SUFFIX% %RESVG_PATH% %UCRT_DIR% %CRT_DIR% %QT_PATH%\bin
+%ZIP_CMD% -9r ..\%PROJECT%%SUFFIX%.zip %PROJECT%%SUFFIX%
+rmdir /S /Q build_msi 2>nul >nul
+move %PROJECT%%SUFFIX% build_msi
+heat dir build_msi -cg ApplicationFiles -dr INSTALLLOCATION -gg -scom -sfrag -srd -sreg -svb6 -out appfiles.wxs
+candle -out appfiles.wixobj appfiles.wxs -arch %ARCH%
+candle -out common.wixobj ..\src\ImageViewer\resources\platform\windows\common.wxs -arch %ARCH%
+candle -out main.wixobj ..\src\ImageViewer\resources\platform\windows\w10_%ARCH%.wxs -ext WixUIExtension -ext WixUtilExtension -arch %ARCH%
+light -out %PROJECT%.msi -b build_msi main.wixobj appfiles.wixobj common.wixobj -ext WixUIExtension -ext WixUtilExtension -dcl:high
+move %PROJECT%.msi ..\%PROJECT%%SUFFIX%.msi
+cd ..
+
+if NOT "%CI%" == "true" pause
