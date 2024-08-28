9fd3464cca79463bf45184bfb88c12a70d2c7a9f
Peter Zhigalov <peter.zhigalov@gmail.com>
2024-08-28 22:55:39 +0700

    Avoid QString allocation in logging for longjmp compatibility

---
+++ src/QtUtils/src/Utils/Logging.cpp
@@ -0,0 +1,70 @@
+/*
+   Copyright (C) 2024 Peter S. Zhigalov <peter.zhigalov@gmail.com>
+
+   This file is part of the `QtUtils' library.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Logging.h"
+
+#include <cassert>
+
+LoggingContextHelper::LoggingContextHelper(const char * const file, const char * const line, const char * const func)
+    : file(file)
+    , line(line)
+    , func(func)
+{
+    // Memory allocation is prohibited here
+    const size_t bufferSize = sizeof(buffer);
+    size_t i = 0;
+    buffer[i++] = '[';
+    for(const char *c = file; *c != '\0' && i < bufferSize; ++c)
+        buffer[i++] = *c;
+    if(i < bufferSize)
+        buffer[i++] = ':';
+    for(const char *c = line; *c != '\0' && i < bufferSize; ++c)
+        buffer[i++] = *c;
+    if(i < bufferSize)
+        buffer[i++] = ':';
+    for(const char *c = func; *c != '\0' && i < bufferSize; ++c)
+        buffer[i++] = *c;
+    if(i < bufferSize)
+        buffer[i++] = ']';
+    if(i < bufferSize)
+        buffer[i++] = '\0';
+    assert(i < bufferSize);
+    buffer[bufferSize - 1] = '\0';
+}
+
+const char *LoggingContextHelper::cutFileName(const char *const file)
+{
+    // Memory allocation is prohibited here
+    const char *fileName = file;
+    for(const char *c = file; c[0] != '\0'; ++c)
+        if(c[0] == '/' || c[0] == '\\')
+            fileName = c + 1;
+    return fileName;
+}
+
+const char *LoggingContextHelper::cutFuncName(const char *const func)
+{
+    // Memory allocation is prohibited here
+    const char *funcName = func;
+    for(const char *c = func; c[0] != '\0' && c[1] != '\0'; ++c)
+        if(c[0] == ':' && c[1] == ':')
+            funcName = c + 2;
+    return funcName;
+}
+
--- src/QtUtils/src/Utils/Logging.h
+++ src/QtUtils/src/Utils/Logging.h
@@ -25,28 +25,47 @@
 #define LOG_DEBUG   qDebug
 #define LOG_WARNING qWarning
 
+class LoggingContextHelper
+{
+public:
+    LoggingContextHelper(const char *const file, const char *const line, const char *const func);
+    inline const char *toString() const { return buffer; }
+
+    static const char *cutFileName(const char *const file);
+    static const char *cutFuncName(const char *const func);
+
+private:
+    const char *const file;
+    const char *const line;
+    const char *const func;
+    char buffer[256];
+};
+
+#define LOGGING_STRINGIZE_NX(X) #X
+#define LOGGING_STRINGIZE(X) LOGGING_STRINGIZE_NX(X)
+
 #if defined (__FILE_NAME__)
-#define LOGGING_FILE QString::fromLatin1(__FILE_NAME__)
+#define LOGGING_FILE (__FILE_NAME__)
 #elif defined (__FILE__)
-#define LOGGING_FILE QString::fromLatin1(__FILE__).section(QChar::fromLatin1('/'), -1).section(QChar::fromLatin1('\\'), -1)
+#define LOGGING_FILE (LoggingContextHelper::cutFileName(__FILE__))
 #else
-#define LOGGING_FILE QString()
+#define LOGGING_FILE ""
 #endif
 
 #if defined (__LINE__)
-#define LOGGING_LINE QString::fromLatin1("%1").arg(__LINE__)
+#define LOGGING_LINE LOGGING_STRINGIZE(__LINE__)
 #else
-#define LOGGING_LINE QString()
+#define LOGGING_LINE ""
 #endif
 
-//#define LOGGING_FUNC QString::fromLatin1(__func__)
-#define LOGGING_FUNC QString::fromLatin1(__FUNCTION__)
-//#define LOGGING_FUNC QString::fromLatin1(__PRETTY_FUNCTION__)
-//#define LOGGING_FUNC QString::fromLatin1(__FUNCSIG__)
+//#define LOGGING_FUNC (LoggingContextHelper::cutFuncName(__func__))
+#define LOGGING_FUNC (LoggingContextHelper::cutFuncName(__FUNCTION__))
+//#define LOGGING_FUNC (__PRETTY_FUNCTION__)
+//#define LOGGING_FUNC (__FUNCSIG__)
 
-#define LOGGING_CTXQS(QS)   QString::fromLatin1("[%1:%2:%3]:").arg((LOGGING_FILE), (LOGGING_LINE), (QS)).toLatin1().data()
-#define LOGGING_CTXS(S)     LOGGING_CTXQS(QString::fromLatin1(S))
-#define LOGGING_CTX         LOGGING_CTXQS((LOGGING_FUNC).section(QString::fromLatin1("::"), -1))
+#define LOGGING_CTXS(S)     (LoggingContextHelper(LOGGING_FILE, LOGGING_LINE, (S)).toString())
+#define LOGGING_CTXQS(QS)   (LOGGING_CTXQS((QS).toLocal8Bit().data()))
+#define LOGGING_CTX         (LOGGING_CTXS(LOGGING_FUNC))
 
 #endif // QTUTILS_LOGGING_H_INCLUDED
 
