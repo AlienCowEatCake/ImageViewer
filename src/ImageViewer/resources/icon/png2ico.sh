68873168998c2e0b195214f8715d9314339c604a
Peter Zhigalov <peter.zhigalov@gmail.com>
2024-09-04 02:13:07 +0700

    Make app .ico NT 4.0 compatible

--- src/ImageViewer/resources/icon/png2ico.sh
+++ src/ImageViewer/resources/icon/png2ico.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
-convert icon_16.png icon_32.png icon_48.png icon_64.png icon_128.png icon_256.png icon.ico
-
+convert \
+    -type PaletteAlpha -colors 16 -depth 4 icon_16.png icon_32.png icon_48.png icon_64.png icon_96.png icon_128.png \
+    -type PaletteAlpha -colors 256 -depth 8 icon_16.png icon_32.png icon_48.png icon_64.png icon_96.png icon_128.png \
+    -type PaletteAlpha -colors 65536 -depth 8 icon_256.png \
+    icon.ico
