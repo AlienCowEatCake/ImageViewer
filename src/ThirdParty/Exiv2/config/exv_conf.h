de57193466a7919b9e507ff93049480d7b56f0db
Peter Zhigalov <peter.zhigalov@gmail.com>
2025-08-30 03:44:56 +0700

    Update Exiv2 to 0.28.6

--- src/ThirdParty/Exiv2/config/exv_conf.h
+++ src/ThirdParty/Exiv2/config/exv_conf.h
@@ -67,14 +67,14 @@
 #define EXV_PACKAGE_NAME "exiv2"
 
 /* Define to the full name and version of this package. */
-#define EXV_PACKAGE_STRING "exiv2 0.28.5"
+#define EXV_PACKAGE_STRING "exiv2 0.28.6"
 
 /* Define to the version of this package. */
-#define EXV_PACKAGE_VERSION "0.28.5"
+#define EXV_PACKAGE_VERSION "0.28.6"
 
 #define EXIV2_MAJOR_VERSION (0U)
 #define EXIV2_MINOR_VERSION (28U)
-#define EXIV2_PATCH_VERSION (5U)
+#define EXIV2_PATCH_VERSION (6U)
 #define EXIV2_TWEAK_VERSION (U)
 
 // Definition to enable translation of Nikon lens names.
