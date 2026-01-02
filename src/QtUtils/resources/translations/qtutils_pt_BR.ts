143dc661878f9605707d1ca0eddd9fd14fd47d15
Cogumelo <cogumelo@tutamail.com>
2026-01-02 14:49:09 -0300

    update qutils pt_BR

---
+++ src/QtUtils/resources/translations/qtutils_pt_BR.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>FileUtils</name>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="661"/>
+        <source>The specified path was not found</source>
+        <translation>O caminho indicado não foi encontrado</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="675"/>
+        <source>Permission denied</source>
+        <translation>Permissão negada</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="701"/>
+        <source>Could not find mount point for specified path</source>
+        <translation>Não foi possível encontrar o ponto de montagem para o caminho indicado</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="131"/>
+        <location filename="../../src/Utils/FileUtils.cpp" line="710"/>
+        <location filename="../../src/Utils/FileUtils.cpp" line="732"/>
+        <location filename="../../src/Utils/FileUtils.cpp" line="809"/>
+        <location filename="../../src/Utils/FileUtils.cpp" line="815"/>
+        <source>The specified path could not be moved to Trash</source>
+        <translation>O caminho indicado não pôde ser movido para a Lixeira</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="803"/>
+        <source>The specified path does not exist</source>
+        <translation>O caminho indicado não existe</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="339"/>
+        <source>Failed to init BEntry for specified path: %1</source>
+        <translation>Falha ao iniciar BEntry para o caminho indicado: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="147"/>
+        <source>The bundle with identifier `com.apple.CoreServices` was not found.</source>
+        <translation>O conjunto identificado como `com.apple.CoreServices` não foi encontrado.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="158"/>
+        <source>The function with name `FSPathMakeRefWithOptions` was not found.</source>
+        <translation>A função nomeada `FSPathMakeRefWithOptions`  não foi encontrada.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="165"/>
+        <source>The function with name `FSMoveObjectToTrashSync` was not found.</source>
+        <translation>A função nomeada `FSMoveObjectToTrashSync` não foi encontrada.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="172"/>
+        <source>The function with name `GetMacOSStatusCommentString` was not found.</source>
+        <translation>A função nomeada `FSMoveObjectToTrashSync`  não foi encontrada.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="349"/>
+        <source>Failed to get entry_ref for specified path: %1</source>
+        <translation>Falha ao obter entry_ref para o caminho:  %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="359"/>
+        <source>Failed to find Trash: %1</source>
+        <translation>Falha ao encontrar Lixeira: %1</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="369"/>
+        <source>Failed to init BDirectory for %1: %2</source>
+        <translation>Falha ao iniciar BDirectory para %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/FileUtils.cpp" line="394"/>
+        <source>Failed to move entry into trash %1: %2</source>
+        <translation>Falha ao mover entrada para a lixeira %1: %2</translation>
+    </message>
+</context>
+<context>
+    <name>IconThemes</name>
+    <message>
+        <location filename="../../src/Utils/IconThemeManager.cpp" line="68"/>
+        <source>Monochrome</source>
+        <translation>Monocromático</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/IconThemeManager.cpp" line="70"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+</context>
+<context>
+    <name>ImageSaver</name>
+    <message>
+        <location filename="../../src/Utils/ImageSaver.cpp" line="98"/>
+        <source>Images</source>
+        <translation>Imagens</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/ImageSaver.cpp" line="105"/>
+        <source>All Images</source>
+        <translation>Todas as imagens</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/ImageSaver.cpp" line="118"/>
+        <source>Save Image File</source>
+        <translation>Salvar arquivo de imagem</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/ImageSaver.cpp" line="133"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/ImageSaver.cpp" line="133"/>
+        <source>Error: Can&apos;t save file</source>
+        <translation>Erro: Falha ao salvar arquivo</translation>
+    </message>
+</context>
+<context>
+    <name>ThemeManager</name>
+    <message>
+        <location filename="../../src/Utils/ThemeManager.cpp" line="403"/>
+        <source>Restart Required</source>
+        <translation>É preciso reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../../src/Utils/ThemeManager.cpp" line="404"/>
+        <source>The theme change will take effect after a restart of application.</source>
+        <translation>A mudança de tema será aplicada após o reinício a aplicação.</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateManager</name>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="145"/>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="245"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="146"/>
+        <source>An update is already in progress. Check back later.</source>
+        <translation>Uma atualização já está em andamento. Tente novamente mais tarde.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="178"/>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="201"/>
+        <source>Information</source>
+        <translation>Informação</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="181"/>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="204"/>
+        <source>Your version:</source>
+        <translation>Sua versão:</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="179"/>
+        <source>You have the latest version.</source>
+        <translation>Você tem a versão mais recente.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="202"/>
+        <source>New updates are available.</source>
+        <translation>Há atualizações disponíveis.</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="206"/>
+        <source>Latest version:</source>
+        <translation>Última versão:</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="209"/>
+        <source>&amp;Download</source>
+        <translation>&amp;Baixar</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="210"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="211"/>
+        <source>&amp;Skip This Version</source>
+        <translation>&amp;Pular esta versão</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="224"/>
+        <source>Automatically check for updates</source>
+        <translation>Procurar por atualizações automaticamente</translation>
+    </message>
+    <message>
+        <location filename="../../src/Updater/UpdateManager.cpp" line="246"/>
+        <source>Can&apos;t check for updates. Try again later.</source>
+        <translation>Falha ao procurar por atualizações. Tente novamente mais tarde.</translation>
+    </message>
+</context>
+</TS>
