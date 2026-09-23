0e518b53c70e2a06f0b03682052693a9fabf87b0
Peter Zhigalov <peter.zhigalov@gmail.com>
2026-09-24 00:36:34 +0700

    Update libyuv to latest main (a09d909)

--- src/ThirdParty/libyuv/libyuv/include/libyuv/row.h
+++ src/ThirdParty/libyuv/libyuv/include/libyuv/row.h
@@ -86,8 +86,7 @@ extern "C" {
 #define HAS_ARGBGRAYROW_SSSE3
 #define HAS_ARGBLUMACOLORTABLEROW_SSSE3
 #define HAS_ARGBMIRRORROW_SSE2
-// TODO: Re-enable once rounding behaviour is fixed.
-// #define HAS_ARGBMULTIPLYROW_SSE2
+#define HAS_ARGBMULTIPLYROW_SSE2
 #define HAS_ARGBPOLYNOMIALROW_SSE2
 #define HAS_ARGBQUANTIZEROW_SSE2
 #define HAS_ARGBSEPIAROW_SSSE3
@@ -187,8 +186,7 @@ extern "C" {
 
 // Effects:
 #define HAS_ARGBADDROW_AVX2
-// TODO: Re-enable once rounding behaviour is fixed.
-// #define HAS_ARGBMULTIPLYROW_AVX2
+#define HAS_ARGBMULTIPLYROW_AVX2
 #define HAS_ARGBSUBTRACTROW_AVX2
 #define HAS_BLENDPLANEROW_AVX2
 #endif
@@ -629,14 +627,17 @@ extern "C" {
 #define HAS_ARGBTOYMATRIXROW_SVE2
 #define HAS_AYUVTOUVROW_SVE2
 #define HAS_AYUVTOVUROW_SVE2
-// #define HAS_CONVERT16TO8ROW_SVE2  // Disabled: NEON version is faster for 128 bit vectors.
+// Disabled: NEON version is faster for 128 bit vectors.
+// #define HAS_CONVERT16TO8ROW_SVE2
 #define HAS_CONVERT8TO8ROW_SVE2
 #define HAS_COPYROW_SVE2
 #define HAS_DIVIDEROW_16_SVE2
 #define HAS_HALFFLOATROW_SVE2
 #define HAS_HALFMERGEUVROW_SVE2
-// #define HAS_HALFROW_16TO8_SVE2  // Disabled: NEON version is faster for 128 bit vectors.
-// #define HAS_HALFWIDTHROW_16TO8_SVE2  // Disabled: NEON version is faster for 128 bit vectors.
+// Disabled: NEON version is faster for 128 bit vectors.
+// #define HAS_HALFROW_16TO8_SVE2
+// Disabled: NEON version is faster for 128 bit vectors.
+// #define HAS_HALFWIDTHROW_16TO8_SVE2
 #define HAS_I210ALPHATOARGBROW_SVE2
 #define HAS_I210TOAR30ROW_SVE2
 #define HAS_I210TOARGBROW_SVE2
@@ -759,8 +760,7 @@ extern "C" {
 #define HAS_ARGBTORGB565ROW_LSX
 #define HAS_ARGBTORGB565DITHERROW_LSX
 #define HAS_ARGBMIRRORROW_LSX
-// TODO: Re-enable once rounding behaviour is fixed.
-// #define HAS_ARGBMULTIPLYROW_LSX
+#define HAS_ARGBMULTIPLYROW_LSX
 #define HAS_I400TOARGBROW_LSX
 #define HAS_I444TOARGBROW_LSX
 #define HAS_INTERPOLATEROW_LSX
@@ -818,8 +818,7 @@ extern "C" {
 #define HAS_ARGBATTENUATEROW_LASX
 #define HAS_ARGBGRAYROW_LASX
 #define HAS_ARGBMIRRORROW_LASX
-// TODO: Re-enable once rounding behaviour is fixed.
-// #define HAS_ARGBMULTIPLYROW_LASX
+#define HAS_ARGBMULTIPLYROW_LASX
 #define HAS_ARGBSEPIAROW_LASX
 #define HAS_ARGBSHADEROW_LASX
 #define HAS_ARGBSHUFFLEROW_LASX
@@ -862,23 +861,10 @@ extern "C" {
 #endif
 
 #if !defined(LIBYUV_DISABLE_RVV) && defined(__riscv_vector)
-#if defined(__riscv_v_intrinsic) && __riscv_v_intrinsic >= 100000
-// Since v1.0, vcreate intrinsic is introduced
-#define LIBYUV_RVV_HAS_VCREATE
-#endif
-#if defined(__riscv_v_intrinsic) && __riscv_v_intrinsic >= 12000
-// Since v0.12, TUPLE_TYPE is introduced for segment load and store.
-#define LIBYUV_RVV_HAS_TUPLE_TYPE
-// Since v0.12, VXRM(fixed-point rounding mode) is included in arguments of
-// fixed-point intrinsics.
-#define LIBYUV_RVV_HAS_VXRM_ARG
-#endif
-#endif
-
-#if !defined(LIBYUV_DISABLE_RVV) && defined(__riscv_vector)
 #define HAS_AR64TOARGBROW_RVV
 #define HAS_ARGBCOPYYTOALPHAROW_RVV
 #define HAS_ARGBEXTRACTALPHAROW_RVV
+#define HAS_ARGBMULTIPLYROW_RVV
 #define HAS_ARGBTOAR64ROW_RVV
 #define HAS_ARGBTOUV444MATRIXROW_RVV
 #define HAS_ARGBTOUVMATRIXROW_RVV
@@ -899,20 +885,6 @@ extern "C" {
 #define HAS_SPLITUVROW_RVV
 #define HAS_SPLITXRGBROW_RVV
 #define HAS_SWAPUVROW_RVV
-
-// The following are available for RVV v0.11 and RVV v1.0
-// TODO(fbarchard): Port to RVV v0.12 (tuple)
-// missing support for vcreate_v:
-//  __riscv_vcreate_v_u16m2x2
-//  __riscv_vcreate_v_u16m2x4
-//  __riscv_vcreate_v_u16m4x2
-//  __riscv_vcreate_v_u8m1x3
-//  __riscv_vcreate_v_u8m1x4
-//  __riscv_vcreate_v_u8m2x2
-//  __riscv_vcreate_v_u8m2x3
-//  __riscv_vcreate_v_u8m2x4
-//  __riscv_vcreate_v_u8m4x2
-#if defined(LIBYUV_RVV_HAS_VCREATE)
 #define HAS_AB64TOARGBROW_RVV
 #define HAS_AR64TOAB64ROW_RVV
 #define HAS_ARGBATTENUATEROW_RVV
@@ -946,7 +918,6 @@ extern "C" {
 #define HAS_RAWTORGBAROW_RVV
 #define HAS_RGB24TOARGBROW_RVV
 #define HAS_RGBATOARGBROW_RVV
-#endif
 #endif
 
 #if defined(_MSC_VER) && !defined(__CLR_VER) && !defined(__clang__)
@@ -5503,6 +5474,10 @@ void ARGBMultiplyRow_Any_LASX(const uint8_t* y_buf,
                               const uint8_t* uv_buf,
                               uint8_t* dst_ptr,
                               int width);
+void ARGBMultiplyRow_RVV(const uint8_t* src_argb,
+                         const uint8_t* src_argb1,
+                         uint8_t* dst_argb,
+                         int width);
 
 // ARGB add images.
 void ARGBAddRow_C(const uint8_t* src_argb,
